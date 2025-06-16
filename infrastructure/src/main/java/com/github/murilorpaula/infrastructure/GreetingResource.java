cb1a5fc79a1c10f68ce07ae0343cc15608bfb707
Murilo de Paula <murilosp.net@gmail.com>
2025-06-16 10:52:30 -0300

    Make GreetingResource a CDI bean

--- infrastructure/src/main/java/com/github/murilorpaula/infrastructure/GreetingResource.java
+++ infrastructure/src/main/java/com/github/murilorpaula/infrastructure/GreetingResource.java
@@ -2,8 +2,13 @@ package com.github.murilorpaula.infrastructure;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
+@ApplicationScoped
+@Produces(MediaType.TEXT_PLAIN)
 public class GreetingResource {
     @GET
     public String hello() {
