36e899612debb53d95ec9c3a1d8b1bece83dcc9a
Murilo de Paula <murilosp.net@gmail.com>
2025-06-17 14:20:33 -0300

    - Criando workaround para gerar link de download das dependencias

--- generate-dependencies-download-links.sh
+++ generate-dependencies-download-links.sh
@@ -1,19 +1,5 @@
-INPUT="./infrastructure/dependencies.txt"
-OUTPUT="dependencies-download-links.txt"
-
-> $OUTPUT
-
-while IFS=: read -r groupId artifactId packaging version scope
-do
-  if [[ "$groupId" != "" && "$artifactId" != "" && "$version" != "" ]]; then
-    groupPath=$(echo $groupId | tr '.' '/')
-    baseUrl="https://repo.maven.apache.org/maven2/${groupPath}/${artifactId}/${version}"
-    jarUrl="${baseUrl}/${artifactId}-${version}.${packaging}"
-    pomUrl="${baseUrl}/${artifactId}-${version}.pom"
-
-    echo $jarUrl >> $OUTPUT
-    echo $pomUrl >> $OUTPUT
-  fi
-done < <(grep ':' $INPUT)
-
-echo "Links gerados em $OUTPUT"
+(cd ~/.m2/repository &&
+find . -type f | while read file; do
+  url="https://repo.maven.apache.org/maven2/$(echo $file | sed 's#^\./##' | tr '\\' '/' )"
+  echo "$url"
+done) > dependencies-download-links.txt
