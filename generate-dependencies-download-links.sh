c75289239a815624e05bfd2c21248bafd29e6c31
Murilo de Paula <murilosp.net@gmail.com>
2025-06-17 14:31:25 -0300

    - Remoção de workaround para rodar no Codex (não tem solução, maven não consegue acessar a internet)

--- generate-dependencies-download-links.sh
+++
@@ -1,5 +0,0 @@
-(cd ~/.m2/repository &&
-find . -type f | while read file; do
-  url="https://repo.maven.apache.org/maven2/$(echo $file | sed 's#^\./##' | tr '\\' '/' )"
-  echo "$url"
-done) > dependencies-download-links.txt
